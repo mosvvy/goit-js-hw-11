d957477cc98bda611fe80459254635f145a72a73
ValeriiaM <moskalenkovvy@gmail.com>
2025-09-20 16:09:32 +0300

    add loader from lib

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,7 +2,7 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryContainer = document.querySelector('.gallery');
-const loader = document.querySelector('.loader');
+const loader = document.querySelector('.loader-container');
 
 const gallery = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
