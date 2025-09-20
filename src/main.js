b8d14f9cc369625cc8873589e76012c485994a10
ValeriiaM <moskalenkovvy@gmail.com>
2025-09-20 15:53:57 +0300

    task realised

--- src/main.js
+++ src/main.js
@@ -7,3 +7,52 @@
 
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+import getImagesByQuery from './js/pixabay-api';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions';
+
+const form = document.querySelector('.form');
+let images = [];
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  clearGallery();
+  const searchReq = event.target.elements.searchText.value.trim();
+  if (!searchReq.length) {
+    showPopUp('Enter key words to search for images');
+    return;
+  }
+
+  showLoader();
+  getImagesByQuery(searchReq)
+    .then(data => {
+      images = data.hits;
+      hideLoader();
+      createGallery(images);
+      if (!images.length) {
+        showPopUp(
+          'Sorry, there are no images matching your search query. Please try again!'
+        );
+        return;
+      }
+    })
+    .catch(error => {
+      showPopUp(error.message);
+    });
+});
+
+function showPopUp(msg) {
+  iziToast.show({
+    message: msg,
+    position: 'topRight',
+    backgroundColor: '#EF4040',
+    messageColor: '#FFFFFF',
+    progressBarColor: '#B51B1B',
+    overlayColor: '#FFBEBE',
+  });
+}
